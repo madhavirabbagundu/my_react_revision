e45c698767ed554ac44a27a84d638feb3caf6397
Madhavi Rabbagundu <madhavirabbagundu@gmail.com>
2023-11-30 21:40:40 +0530

    hooks

--- react-coding/src/App.js
+++ react-coding/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  const [data,setData] = React.useState({
+    text:"",
+    
+  })
+
+  const handleChnage =(e)=>{
+const {name,value} = e.target;
+setData({...data,[name]:value})
+  }
+  const handleSubmit = (e)=>{
+    console.log(data)
+
+e.preventDefault()
+  }
+  const {text} = data
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1> input taking data:: {data.text}</h1>
+     <form onSubmit={handleSubmit}>
+   <input type = "text"
+   placeholder='Enter something'
+   onChange={handleChnage}
+   value= {text}
+   name = "text"/>
+   <button type = "submit" value = "submit">Update</button>
+   </form>
     </div>
   );
 }
