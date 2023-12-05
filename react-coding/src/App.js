770d0356cbae18c83c3db059293ef3d04441d4cf
Madhavi Rabbagundu <madhavirabbagundu@gmail.com>
2023-12-05 22:47:16 +0530

    input boxes using the context api

--- react-coding/src/App.js
+++ react-coding/src/App.js
@@ -6,10 +6,13 @@ import {ContextChild} from './Components/ContextChild'
 
 export const userContext = createContext(null);
 function App() {
-  const [count,setCount] = React.useState("Madhavi")
+  const [count,setCount] = React.useState(0);
+  const [input,setInput] = React.useState("")
+  const [show,setShow] = React.useState(false);
+
 
   return(
- <userContext.Provider value = {{count,setCount}}>
+ <userContext.Provider value = {{count,setCount,input,setInput,setShow,show}}>
   <div>
 <ContextChild />
 </div>
--- react-coding/src/Components/Context.jsx
+++ react-coding/src/Components/Context.jsx
@@ -3,10 +3,28 @@ import { useContext } from "react";
 import { userContext } from "../App";
 function Context(){
     const {count,setCount} = useContext(userContext);
+    const {input,setInput,setShow} = useContext(userContext);
+    // console.log(input,input.name,"input")
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        console.log(input,"main Data")
+
+    }
+    console.log(input,"main")
+
     return(
         <>
-        <h1 style = {{color:'green',fontFamily:"obligue"}}>{count}</h1>
-        <button onClick = {()=>setCount("Chaithu,deepika,baba,bharath")}>Change</button>
+        <h1 style = {{color:'green',fontFamily:"inte"}}>{count}</h1>
+        <button onClick = {()=>setCount(count+1)}>INC</button>
+        <button onClick = {()=>setCount(count-1)}>DEC</button>
+        <div>
+            <form onSubmit={handleSubmit}>
+          <input type = "text" placeholder="Enter your name"  onChange = {(e)=>{(setInput(e.target.value))}}/>
+          {/* <input type = "number" placeholder="Enter your number"onChange = {(e)=>{(setInput(e.target.value))}}/> */}
+          <button type = "submit" value = 'submit'onClick = {()=>{setShow(true)}}>Submit</button>
+          </form>
+        </div>
+        <div>Name:{input}</div>
 
         </>
     )
